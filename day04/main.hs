07c92c633a7e31859d273615bcda9ff853271478
Niels Disveld <nielsdisveld@gmail.com>
2024-12-04 22:25:29 +0100

    add state monad for adding increasing int ids

--- day04/main.hs
+++ day04/main.hs
@@ -1,8 +1,6 @@
 import Control.Monad (unless)
-import Data.List (transpose)
-import qualified Data.List as List
-
-simple = "..X...\n.SAMX.\n.A..A.\nXMAS.S\n.X...."
+import Control.Monad.State (State, get, modify, runState)
+import Data.List (filter, group, sort, transpose)
 
 testInput = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX"
 
@@ -10,51 +8,81 @@ main :: IO ()
 main =
   do
     unless check1 (fail "part 1 oof")
-    -- unless check2 (fail "part 2 oof")
+    unless check2 (fail "part 2 oof")
     input <- fmap lines getContents
-    print (run input)
+    print (part1 input)
+    print (part2 input)
 
 -- test example input
-check1 = run (lines testInput) == 18
+check1 = part1 (lines testInput) == 18
 
-check2 = True
+check2 = part2 (lines testInput) == 9
 
 -- run
-run xss =
+part1 xss =
   countLines xss
     + countCols xss
     + countLines (diagonals xss)
     + countLines (diagonals (flipHor xss))
 
---count
+part2 xss =
+  let withIds = addIds xss
+      diags = diagonals withIds
+      diags2 = diagonals (flipHor withIds)
+      feasible = findAllMas diags ++ findAllMas diags2
+   in (length . filter (2 ==) . fmap length . group . sort) feasible
+
+-- count (part1)
 countCols = countLines . transpose
 
-countLines = sum . fmap countLine
+countLines = sum . fmap countXmas
 
-countLine ('X' : 'M' : 'A' : 'S' : xs) = 1 + countLine ('S' : xs) -- 'S' can be start of next word
-countLine ('S' : 'A' : 'M' : 'X' : xs) = 1 + countLine ('X' : xs) -- vice versa
-countLine (_ : xs) = countLine xs
-countLine [] = 0
+countXmas ('X' : 'M' : 'A' : 'S' : xs) = 1 + countXmas ('S' : xs) -- 'S' can be start of next word
+countXmas ('S' : 'A' : 'M' : 'X' : xs) = 1 + countXmas ('X' : xs) -- vice versa
+countXmas (_ : xs) = countXmas xs
+countXmas [] = 0
 
---find
--- findMas (( 'M',_  ): 'A' : 'S' : xs) =
+-- find (part2)
+findAllMas = concatMap (findMas [])
+
+findMas acc (('M', _) : ('A', n1) : ('S', n2) : xs) = findMas (n1 : acc) (('S', n2) : xs)
+findMas acc (('S', _) : ('A', n1) : ('M', n2) : xs) = findMas (n1 : acc) (('M', n2) : xs)
+findMas acc (_ : xs) = findMas acc xs
+findMas acc [] = acc
+
 -- helpers
-withIds n (x : xs) =
-  let (n', xs') = withIds (n + 1) xs
-   in (n', (x, n) : xs')
-withIds n [] = (n, [])
-
-gridWithIds n (xs : xss) =
-  let (n', ys) = withIds n xs
-   in ys : gridWithIds n' xss
-gridWithIds n [] = []
-
 flipHor = map reverse
 
+-- diagonals going from top left to bottom right
 diagonals [] = []
-diagonals ((x : xs) : yss) = [x] : zipList xs (diagonals yss)
+diagonals ((x : xs) : yss) = [x] : zipDiagonals xs (diagonals yss)
 diagonals ([] : yss) = yss
 
-zipList [] yss = yss
-zipList xs [] = fmap (: []) xs
-zipList (x : xs) (ys : yss) = (x : ys) : zipList xs yss
+-- similar to 'zipwith (:)' but does not return empty list when one of the list arguments exhausts
+zipDiagonals [] yss = yss
+zipDiagonals xs [] = fmap (: []) xs
+zipDiagonals (x : xs) (ys : yss) = (x : ys) : zipDiagonals xs yss
+
+-- Use state monad to add ids to every element of the grid (Mostly as exercise)
+addIds xss =
+  let (yss, _) = runState (travLines xss) 0
+   in yss
+
+increment :: State Int Int = do
+  n <- get
+  modify (+ 1)
+  pure n
+
+travLines :: [[a]] -> State Int [[(a, Int)]]
+travLines (xs : xss) = do
+  ys <- travLine xs
+  yss <- travLines xss
+  pure (ys : yss)
+travLines [] = pure []
+
+travLine :: [a] -> State Int [(a, Int)]
+travLine (x : xs) = do
+  n <- increment
+  ys <- travLine xs
+  pure ((x, n) : ys)
+travLine [] = pure []
