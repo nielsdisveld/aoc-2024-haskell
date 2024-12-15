1c482937ae051b12ae230163f185068993ead165
Niels Disveld <disveld@floydhamilton.nl>
2024-12-15 15:40:58 +0100

    day13 part2

--- day13/main.hs
+++ day13/main.hs
@@ -1,9 +1,9 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 import Control.Monad (unless)
-import Data.Int (Int64)
 import Data.Maybe (catMaybes, mapMaybe)
 import GHC.Base (maxInt)
+import GHC.Real (Fractional ((/)))
 
 testInput = parse "Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279"
 
@@ -12,7 +12,7 @@ main =
     unless check1 (fail "part 1 oof")
     input <- fmap parse getContents
     print (part1 input)
-    print (part2 testInput)
+    print (part2 input)
 
 --- test example input
 check1 = part1 testInput == 480
@@ -24,41 +24,39 @@ parseSegment segment =
   let [p1, p2, p3] = fmap parseLine segment
    in (p1, p2, p3)
 
-parseLine :: String -> (Int64, Int64)
+parseLine :: String -> (Integer, Integer)
 parseLine str =
   let (x, y) = case words str of
         [_, _, x, y] -> (x, y)
         [_, x, y] -> (x, y)
-   in (read $ init $ drop 2 x, read $drop 2 y)
+   in (read $ init $ drop 2 x, read $ drop 2 y)
 
 --- solve
-part1 = sum . mapMaybe solveSegment
+part1 = sum . map score . mapMaybe solveEq
 
-part2 (x : xs) = sum $ mapMaybe solveSegmentPart2 [x]
+part2 = sum . map score . mapMaybe solveEqPart2
 
-solveSegmentPart2 (p1, p2, (x2, y2)) =
-  solveSegment (p1, p2, (x2 + 10000000000000, y2 + 10000000000000))
+solveEqPart2 (p1, p2, (x3, y3)) =
+  solveEq (p1, p2, (x3 + 10000000000000 :: Integer, y3 + 10000000000000 :: Integer))
 
-solveSegment ((x1, y1), p2, (x2, y2)) =
-  let pairs = mapMaybe (solveLine (x1, y1) p2 (x2, y2)) [0 .. 1 + (x2 `div` x1)]
-      scores = fmap score pairs
-   in case scores of
-        [] -> Nothing
-        (s : ss) -> Just $ foldl min s ss
-
-score :: (Int64, Int64) -> Int64
+score :: (Integer, Integer) -> Integer
 score (a, b) = (3 * a) + b
 
-solveLine (x1, y1) p2 (x2, y2) a =
-  let mb = solveB p2 (x2 - (a * x1), y2 - (a * y1))
-   in mb >>= \b -> Just (a, b)
+solveEq ((x1, y1), (x2, y2), p3)
+  | determinant (x1, x2, y1, y2) /= 0 = solveWithInverse (x1, x2, y1, y2) p3
 
-solveB (x1, y1) (x2, y2) =
-  let k = x2 `div` x1
-      b
-        | x2 `mod` x1 == 0 && k * y1 == y2 = Just k
-        | otherwise = Nothing
-   in b
+-- matrix helpers
+determinant (a, b, c, d) = a * d - (b * c)
+
+solveWithInverse (a, b, c, d) (x, y) =
+  let (x', y') = (d * x - (b * y), (- c * x) + (a * y))
+      det = determinant (a, b, c, d)
+   in if x' `mod` det == 0
+        && y' `mod` det == 0
+        && det * x' >= 0
+        && det * y' >= 0
+        then Just (x' `div` det, y' `div` det)
+        else Nothing
 
 --- helpers
 unintersperse :: Eq a => a -> [a] -> [[a]]
