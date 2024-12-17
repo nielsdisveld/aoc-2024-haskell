d7af2401c85cfa6d7ded7771c02457ece610b23e
Niels Disveld <disveld@floydhamilton.nl>
2024-12-17 16:26:08 +0100

    day16 part2

--- day16/main.hs
+++ day16/main.hs
@@ -2,6 +2,7 @@
 
 import Control.Monad (unless)
 import Data.Foldable (minimumBy)
+import Data.List (nub, sort)
 import qualified Data.Map as Map
 import Data.Maybe (fromJust, fromMaybe, isJust, isNothing)
 import qualified Data.Set as Set
@@ -15,14 +16,21 @@ testInput2 = parse "#################\n#...#...#...#..E#\n#.#.#.#.#.#.#.#.#\n#.#
 main =
   do
     unless check1 (fail "part 1 oof")
-    unless check2 (fail "part 2 oof")
+    unless check2 (fail "part 1 oof")
+    unless check3 (fail "part 2 oof")
+    unless check4 (fail "part 2 oof")
     input <- fmap parse getContents
-    print (dijkstra input)
+    print (part1 input)
+    print (part2 input)
 
 --- test example input
-check1 = dijkstra testInput1 == 7036
+check1 = part1 testInput1 == 7036
 
-check2 = dijkstra testInput2 == 11048
+check2 = part1 testInput2 == 11048
+
+check3 = part2 testInput1 == 45
+
+check4 = part2 testInput2 == 64
 
 --- parse
 parse = toVertices . parseGrid 0 0 [] [] [] . lines
@@ -42,29 +50,50 @@ toVertices (tiles, source, target) =
    in (addTile [(target, (0, 0))] tiles, source, target)
 
 --- run
-dijkstra (vs, source, target) =
-  let loop q dist
-        | q == Set.empty = lu dist (target, (0, 0))
+part1 (vs, source, target) =
+  let (dist, _) = dijkstra vs source target
+   in lu dist (target, (0, 0))
+
+part2 (vs, source, target) =
+  let (_, from) = dijkstra vs source target
+   in countTiles source target from
+
+dijkstra vs source target =
+  let loop q dist from
+        | q == Set.empty = (dist, from)
         | otherwise =
-          let next = minimumBy (comp dist) q
+          let next = minimumBy (comp dist) q -- Could use priority queue
               dnext = lu dist next
               q' = Set.delete next q
               neighbors = Set.filter (isNeighbor next) q'
 
-              folder dist' v =
+              folder (dist', from') v =
                 case (Map.lookup v dist', distEdge next v) of
-                  (Nothing, Just d) -> Map.insert v (dnext + d) dist'
-                  (Just d1, Just d2) ->
-                    if d1 <= dnext + d2
-                      then dist'
-                      else Map.insert v (dnext + d2) dist'
+                  (Nothing, Just d) -> (Map.insert v (dnext + d) dist', Map.insert v [next] from')
+                  (Just d1, Just d2)
+                    | d1 < dnext + d2 -> (dist', from')
+                    | d1 == dnext + d2 -> (dist', updateFrom v next from')
+                    | otherwise -> (Map.insert v (dnext + d2) dist', Map.insert v [next] from')
 
-              dist' = foldl folder dist neighbors
-           in loop q' dist'
+              (dist', from') = foldl folder (dist, from) neighbors
+           in loop q' dist' from'
+
       withStart = Map.insert (source, (1, 0)) 0 Map.empty
-   in loop vs withStart
+   in loop vs withStart Map.empty
+
+-- score
+countTiles source target from =
+  let loop tiles p
+        | fst p == source = tiles
+        | otherwise = concatMap (loop (p : tiles)) (lu from p)
+   in length $ nub $ sort $ (:) source $map fst $ loop [] (target, (0, 0))
 
 --- helpers
+updateFrom k v from =
+  case Map.lookup k from of
+    Just lst -> Map.insert k (v : lst) from
+    Nothing -> Map.insert k [v] from
+
 lu mp x = fromJust (Map.lookup x mp)
 
 (+.) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
